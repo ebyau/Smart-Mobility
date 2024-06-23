b6a910aca85b290c8b1ebccce21c1d42a8127975
ebyau <bebyau@gmail.com>
2024-06-23 11:00:08 +0200

    fixed video frames overwritting other frames

--- extract-video-frames/extract_frames.py
+++ extract-video-frames/extract_frames.py
@@ -18,6 +18,7 @@ Usage:
 
 import cv2
 import os
+from tqdm.auto import tqdm
 
 def create_output_directory(output_directory):
     """
@@ -27,10 +28,11 @@ def create_output_directory(output_directory):
         output_directory (str): Path to the directory where extracted frames will be saved.
     """
     if not os.path.exists(output_directory):
-        print(f'Created output directory {output_directory}...')
+        #print(f'[INFO] Created output directory {output_directory}...')
         os.makedirs(output_directory)
     else:
-        print(f'Directory already exists {output_directory}...')
+        pass
+        #print(f'[INFO] Output directory already exists')
 
 def extract_frames_from_video(video_path, output_directory, frame_rate=1):
     """
@@ -62,7 +64,8 @@ def extract_frames_from_video(video_path, output_directory, frame_rate=1):
         
         # Save frame if it is at the specified interval
         if frame_count % frame_interval == 0:
-            frame_filename = os.path.join(output_directory, f"frame_{extracted_frame_count:05d}.jpg")
+            video_filename = os.path.splitext(os.path.basename(video_path))[0]
+            frame_filename = os.path.join(output_directory, f"frame_{video_filename}_{extracted_frame_count:06d}.jpg")
             cv2.imwrite(frame_filename, frame)
             extracted_frame_count += 1
             
@@ -79,15 +82,30 @@ def process_video_in_folder(input_folder, output_directory, frame_rate):
         output_directory (str): Directory where extracted frames will be saved.
         frame_rate (int): Number of frames to extract per second.
     """
-    for video_filename in os.listdir(input_folder):
-        if video_filename.endswith(('.mp4', '.avi', '.mov', '.mkv')):
+    
+    video_files = [file for file in os.listdir(input_folder) if file.endswith(('.mp4','.avi','.mov','.mkv'))]
+    with tqdm(total=len(video_files), desc=f'Extracting Videos') as pbar:
+        for count, video_filename in enumerate(video_files, 1):
             video_path = os.path.join(input_folder, video_filename)
-            video_output_folder = os.path.join(output_directory, os.path.splitext(video_filename)[0])
-            extract_frames_from_video(video_path, video_output_folder, frame_rate)
+            extract_frames_from_video(video_path, output_directory, frame_rate)
+            #pbar.set_description(f'Extracting Video {count + 1} / {len(video_files)}')
+            pbar.update(1)
+
+    
+    # with tqdm(total=len(os.listdir(input_folder)), desc=f'Extracting Video {iter + 1} / {len(os.listdir(input_folder))}') as pbar:
+    #     for video_filename in os.listdir(input_folder):
+    #         if video_filename.endswith(('.mp4', '.avi', '.mov', '.mkv')):
+    #             video_path = os.path.join(input_folder, video_filename)
+    #             #video_output_folder = os.path.join(output_directory)
+    #             #print(f"Extracting frames for {video_filename}......")
+    #             extract_frames_from_video(video_path, output_directory, frame_rate)
+    #         iter += 1
+    #         pbar.update(1)
+    #     #print('[INFO] Operation completed successfully')
 
 # Set parameters
-input_folder = 'kabisa-videos'
-output_folder = 'extracted_frames'
+input_folder = '/home/ebiyau/workspaces/Smart-Mobility/dashcam-videos'
+output_folder = '/home/ebiyau/workspaces/Smart-Mobility/dashcam-frames'
 frame_rate = 1
 
 # Process the videos
